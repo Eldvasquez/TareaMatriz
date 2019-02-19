a02f73c325a39f04217eeb367e7d3bc6e7de616d
Eldvasquez <eld.vasquez@gmail.com>
2019-02-18 19:33:31 -0600

    Actualizacion

--- src/TareaMatriz4/tareaMatriz.java
+++ src/TareaMatriz4/tareaMatriz.java
@@ -11,13 +11,13 @@ public class tareaMatriz {
 	
 	
 	 
-	public static String Matriz[][]= new String [10][7];
+	public static String Matriz[][]= new String [10][8];
 	public static float Departamento[]= new float [5];
 	public static Scanner entrada= new Scanner (System.in);
 	public static DecimalFormat dc= new DecimalFormat("#.00");
-	public static String deducciones,percepciones,departamento,sueldoBase,Sisr;
-	public static int opcion, ded,perc,dep=0,sB;
-	public static float isr=0,sueldoLiquido=0,total;
+	public static String deducciones,percepciones,departamento,sueldoBase;
+	public static int opcion, ded,perc,dep=0,sB,igss;
+	public static float isr=0,sueldoLiquido=0,total,floatIgss;
 	
 	public static int NoDepartamento(int i) {
 		dep=(int)(1+(Math.random()*5));
@@ -27,7 +27,7 @@ public class tareaMatriz {
 	public static void DatosMatriz() {
 		for(int i=0;i<10;i++) {
 			//
-			 Matriz[i][0]=JOptionPane.showInputDialog(null,"Ingrese el nombre:"+(i+1));
+		     Matriz[i][0]=JOptionPane.showInputDialog(null,"Ingrese el nombre:"+(i+1));
 			//
 			sB=(int)(2500+(Math.random()*5000));
 			sueldoBase=Integer.toString(sB);
@@ -35,27 +35,30 @@ public class tareaMatriz {
 			//
 			ded=(int)(1+(Math.random())*2500);
 			deducciones=Integer.toString(ded);
-			Matriz[i][2]=deducciones;
+			Matriz[i][2]=deducciones;			
 			//
 			perc=(int) (1+(Math.random())*2500);
 			percepciones=Integer.toString(perc);
 			Matriz[i][3]=percepciones;
 			//
+			floatIgss=(float) ((sB+ perc )*0.0483);
+			Matriz[i][4]=String.valueOf(dc.format(floatIgss));
+			//
 			if(sB>=2500 && sB<=5000) {
 				isr=(float) ((sB) * 0.03);
-				Matriz[i][4]=String.valueOf(dc.format(isr));
+				Matriz[i][5]=String.valueOf(dc.format(isr));
 			}else if (sB>=5001 && sB<=10000) {
 				isr=(float) ((sB) * 0.05);
-						Matriz[i][4]=String.valueOf(dc.format(isr));
+						Matriz[i][5]=String.valueOf(dc.format(isr));
 			}else if (sB>=10000 && sB<=100000) {
 				isr=(float) ((sB) * 0.10);
-						Matriz[i][4]=String.valueOf(dc.format(isr));
+						Matriz[i][5]=String.valueOf(dc.format(isr));
 			}
 			//
 			sueldoLiquido=sB-ded+perc-isr;
-			Matriz[i][5]=String.valueOf(dc.format(sueldoLiquido));
+			Matriz[i][6]=String.valueOf(dc.format(sueldoLiquido));
 			//
-			Matriz[i][6]=Integer.toString(NoDepartamento(dep));
+			Matriz[i][7]=Integer.toString(NoDepartamento(dep));
 			
 		}
 	}
@@ -63,24 +66,24 @@ public class tareaMatriz {
 	public static void MostrarDepartamento() {
 	      for(int i = 0; i<10;i++)
 	      { 
-	    	  total = Float.parseFloat(Matriz[i][5]);
-	      if(Matriz[i][6].equals("1"))
+	    	  total = Float.parseFloat(Matriz[i][6]);
+	      if(Matriz[i][7].equals("1"))
 	      {
 	    	  Departamento[0] = Departamento[0] + total;
 	      }
-	      if(Matriz[i][6].equals("2"))
+	      if(Matriz[i][7].equals("2"))
 	      {
 	    	  Departamento[1] = Departamento[1] + total;
 	      }
-	       if(Matriz[i][6].equals("3"))
+	       if(Matriz[i][7].equals("3"))
 	      {
 	    	   Departamento[2] = Departamento[2] + total;
 	      }
-	      if(Matriz[i][6].equals("4"))
+	      if(Matriz[i][7].equals("4"))
 	      {
 	    	  Departamento[3] = Departamento[3] + total;
 	      }
-	      if(Matriz[i][6].equals("5"))
+	      if(Matriz[i][7].equals("5"))
 	      {
 	    	  Departamento[4] = Departamento[4] + total;
 	      }
@@ -98,12 +101,12 @@ public class tareaMatriz {
 	}
 	
 	public static void MostrarMatriz() {
-		System.out.println(" --------------------------------------------------------------------------------------------------------------");
-		System.out.println("|Nombres------Sueldo Base------Deducciones------Percepciones------ISR------Sueldo Liquido------No Departamento|\n");
+		System.out.println(" -------------------------------------------------------------------------------------------------------------------------------");
+		System.out.println("|Nombres------Sueldo Base-----Deducciones-----Percepciones-----IGSS---------ISR---------Sueldo Liquido------No Departamento|\n");
 		
 		for(int j=0;j<10;j++) {
-			System.out.println("|"+Matriz[j][0]+"\t\t"+Matriz[j][1]+"\t\t"+Matriz[j][2]+"\t\t"+Matriz[j][3]+"\t\t"+Matriz[j][4]+"\t\t"+Matriz[j][5]+"\t\t\t"+Matriz[j][6]+"   |");
-		System.out.println(" --------------------------------------------------------------------------------------------------------------");
+			System.out.println("|"+Matriz[j][0]+"\t\t"+Matriz[j][1]+"\t\t"+Matriz[j][2]+"\t\t"+Matriz[j][3]+"\t\t"+Matriz[j][4]+"\t\t"+Matriz[j][5]+"\t\t"+Matriz[j][6]+"\t\t\t"+Matriz[j][7]+"    |");
+		System.out.println(" -------------------------------------------------------------------------------------------------------------------------------");
 	}
 	}
 	public static void main(String[] args) {
